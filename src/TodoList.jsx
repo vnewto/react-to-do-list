76fe093d51e75a4fa56b2cb9dba116a80372aa6c
vnewto <181774283+vnewto@users.noreply.github.com>
2025-08-12 12:28:10 -0400

    feat: create ternary statement to display todolist when len > 0

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,11 +3,17 @@ import TodoListItem from './TodoListItem.jsx'
 
 {/*extract from TodoList.jsx*/}
 function TodoList ({todoList}) {
+
     return (
         <>
-            <ul>
-                {todoList.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
-            </ul>
+            {/* create ternary statement that compares todolist length to 0 */}
+            {todoList.length == 0 ? 
+                // if no items, display instructions to add todo
+                <p>Add a todo above to get started</p> : 
+                // if items, display items
+                <ul>
+                    {todoList.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
+                </ul>}
         </>
     );
 }
