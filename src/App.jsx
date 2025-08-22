ff593037ab720321e248be27d37757b1c479967b
vnewto <181774283+vnewto@users.noreply.github.com>
2025-08-21 20:35:50 -0400

    refactor: reorganized directories to prepare for refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import TodoList from './TodoList.jsx';
-import TodoForm from './TodoForm.jsx';
+import TodoList from './features/TodoList/TodoList.jsx';
+import TodoForm from './features/TodoForm.jsx';
 import { useRef } from 'react'
 
 function App() {
