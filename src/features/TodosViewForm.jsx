3474560eb4b880b99a5485eb49d50b2ce536bed1
vnewto <181774283+vnewto@users.noreply.github.com>
2025-09-04 13:15:14 -0400

    fix: add for and id attributes to labels in TodosViewForm

--- src/features/TodosViewForm.jsx
+++ src/features/TodosViewForm.jsx
@@ -30,10 +30,11 @@ export default function TodosViewForm({
 
     // build form with search, sortby, and sortdirection options for displaying todos
     return (
-        <form onSubmit={preventRefresh} id="sort-filter">
+        <form onSubmit={preventRefresh}>
             <div>
-                <label>Search todos</label>
+                <label htmlFor="search">Search todos</label>
                 <input
+                    id="search"
                     type="text"
                     value={queryString}
                     onChange={handleSearchQuery}
@@ -43,15 +44,19 @@ export default function TodosViewForm({
                 </button>
             </div>
             <div>
-                <label>Sort by</label>
+                <label htmlFor="sortBy">Sort by</label>
                 {/* Add an onChange handler that uses an anonymous function that Takes the event object and Calls setSortField with the event target's value. */}
-                <select value={sortField} onChange={handleSortBy}>
+                <select id="sortBy" value={sortField} onChange={handleSortBy}>
                     <option value="title">Title</option>
                     <option value="createdTime">Time added</option>
                 </select>
-                <label>Direction</label>
+                <label htmlFor="sortDir">Direction</label>
                 {/* Add an onChange handler that uses an anonymous function that Takes the event object and Calls setSortDirection with the event target's value. */}
-                <select value={sortDirection} onChange={handleSortDirection}>
+                <select
+                    id="sortDir"
+                    value={sortDirection}
+                    onChange={handleSortDirection}
+                >
                     <option value="asc">Ascending</option>
                     <option value="desc">Descending</option>
                 </select>
