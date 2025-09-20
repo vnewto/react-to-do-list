898f4e53fc97a10c6ddaa58a1c20ddb1fe8eb600
vnewto <181774283+vnewto@users.noreply.github.com>
2025-09-19 20:44:32 -0400

    add overall text styling in App.css file

--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -1,26 +1,41 @@
 // import TodoListItem
-import TodoListItem from './TodoListItem.jsx'
+import TodoListItem from './TodoListItem.jsx';
 
-{/*extract from TodoList.jsx*/}
-function TodoList ({ todoList, onCompleteTodo, onUpdateTodo, isLoading }) {
+//import css classes
+import classes from './TodoList.module.css';
 
-    const filteredTodoList = todoList.filter((todo) => todo.isCompleted == false)
+{
+    /*extract from TodoList.jsx*/
+}
+function TodoList({ todoList, onCompleteTodo, onUpdateTodo, isLoading }) {
+    const filteredTodoList = todoList.filter(
+        (todo) => todo.isCompleted == false
+    );
 
     return (
         <>
             {/* Add a statement that conditionally renders the loading message based on isLoading */}
-            {isLoading ? 
-                <p>Loading your todos...</p> :
-                // create ternary statement that compares todolist length to 0
-                filteredTodoList.length == 0 ? 
-                    // if no items, display instructions to add todo
-                    <p>Add a todo above to get started</p> : 
-                    // if items, display items
-                    <ul>
-                        {filteredTodoList.map(todo => <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo} onUpdateTodo={onUpdateTodo}/>)}
-                    </ul>}
+            {isLoading ? (
+                <p>Loading your todos...</p>
+            ) : // create ternary statement that compares todolist length to 0
+            filteredTodoList.length == 0 ? (
+                // if no items, display instructions to add todo
+                <p>Add a todo above to get started</p>
+            ) : (
+                // if items, display items
+                <ul>
+                    {filteredTodoList.map((todo) => (
+                        <TodoListItem
+                            key={todo.id}
+                            todo={todo}
+                            onCompleteTodo={onCompleteTodo}
+                            onUpdateTodo={onUpdateTodo}
+                        />
+                    ))}
+                </ul>
+            )}
         </>
     );
 }
 
-export default TodoList
+export default TodoList;
