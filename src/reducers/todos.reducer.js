76ed7ea46b8f15739a5883799d6e9d33d95fa5f6
vnewto <181774283+vnewto@users.noreply.github.com>
2025-09-25 10:51:01 -0400

    refactor: declare initialState variable for reducer function

---
+++ src/reducers/todos.reducer.js
@@ -0,0 +1,9 @@
+//Declare an initialState constant and assign it an object with all of the states you are going to use
+const initialState = {
+    todolist: [],
+    isLoading: false,
+    isSaving: false,
+    errorMessage: '',
+};
+
+export {initialState};
