061160c19106907dc8b0ce0a072834a1ca26c065
vnewto <181774283+vnewto@users.noreply.github.com>
2025-09-25 10:58:48 -0400

    refactor: declare actions object for reducer function

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -6,4 +6,24 @@ const initialState = {
     errorMessage: '',
 };
 
-export {initialState};
+//declare actions object for all the instances where state gets updated in App
+const actions = {
+    //actions in useEffect that loads todos
+    fetchTodos: 'fetchTodos',
+    loadTodos: 'loadTodos',
+    //found in useEffect and addTodo to handle failed requests
+    setLoadError: 'setLoadError',
+    //actions found in addTodo
+    startRequest: 'startRequest',
+    addTodo: 'addTodo',
+    endRequest: 'endRequest',
+    //found in helper functions
+    updateTodo: 'updateTodo',
+    completeTodo: 'completeTodo',
+    //reverts todos when requests fail
+    revertTodo: 'revertTodo',
+    //action on Dismiss Error button
+    clearError: 'clearError',
+};
+
+export { initialState, actions };
