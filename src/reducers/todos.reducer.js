65d82380056d5ec0d2439b56d5b59e1c91e38091
vnewto <181774283+vnewto@users.noreply.github.com>
2025-09-25 11:23:06 -0400

    refactor: create reducer function that returns state unchanged for each case

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -26,4 +26,52 @@ const actions = {
     clearError: 'clearError',
 };
 
-export { initialState, actions };
+//declare reducer function that takes in a state and an action, and has switch statements for each case
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case actions.fetchTodos:
+            return {
+                ...state,
+            };
+        case actions.loadTodos:
+            return {
+                ...state,
+            };
+        case actions.setLoadError:
+            return {
+                ...state,
+            };
+        case actions.startRequest:
+            return {
+                ...state,
+            };
+        case actions.addTodo:
+            return {
+                ...state,
+            };
+        case actions.endRequest:
+            return {
+                ...state,
+            };
+        case actions.updateTodo:
+            return {
+                ...state,
+            };
+        case actions.completeTodo:
+            return {
+                ...state,
+            };
+        case actions.revertTodo:
+            return {
+                ...state,
+            };
+        case actions.clearError:
+            return {
+                ...state,
+            };
+        default:
+            return state;
+    }
+}
+
+export { initialState, actions, reducer };
