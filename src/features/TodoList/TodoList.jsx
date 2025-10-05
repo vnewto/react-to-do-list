060b71cb1419939fa9fe3dbfff363e601ce3fd2d
vnewto <181774283+vnewto@users.noreply.github.com>
2025-10-04 20:37:08 -0400

    fix: create useEffect that ensures currentPage is a valid number

--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -1,15 +1,19 @@
 // import TodoListItem
 import TodoListItem from './TodoListItem.jsx';
 
-import { useSearchParams } from 'react-router';
+import { useSearchParams, useNavigate } from 'react-router';
+
+import { useState, useEffect } from 'react';
 
 //import css classes
 import classes from './TodoList.module.css';
 
-{
-    /*extract from TodoList.jsx*/
-}
-function TodoList({ todoList, onCompleteTodo, onUpdateTodo, isLoading }) {
+export default function TodoList({
+    todoList,
+    onCompleteTodo,
+    onUpdateTodo,
+    isLoading,
+}) {
     const filteredTodoList = todoList.filter(
         (todo) => todo.isCompleted == false
     );
@@ -17,9 +21,40 @@ function TodoList({ todoList, onCompleteTodo, onUpdateTodo, isLoading }) {
     //setup for pagination
     const [searchParams, setSearchParams] = useSearchParams();
     const itemsPerPage = 15;
-    const currentPage = (parseInt(searchParams.get('page')) || -1, 10);
+    const [currentPage, setCurrentPage] = useState(
+        parseInt(searchParams.get('page')) || 1
+    );
     const indexOfFirstTodo = (currentPage - 1) * itemsPerPage;
     const totalPages = Math.ceil(filteredTodoList.length / itemsPerPage);
+
+    // Create a handler function handlePreviousPage that uses setSearchParams to set page to currentPage - 1 while preventing the value from decreasing below 1
+    function handlePreviousPage() {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+
+    function handleNextPage() {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+
+    const navigate = useNavigate();
+
+    //Create a useEffect that examines the currentPage
+    useEffect(() => {
+        if (totalPages > 0) {
+            //If it is not a valid number (eg: "moose"), less than 1, or greater than totalPages, programmatically navigate to "/"
+            if (typeof currentPage != 'number') {
+                navigate('/');
+            } else if (currentPage < 1) {
+                navigate('/');
+            } else if (currentPage > totalPages) {
+                navigate('/');
+            }
+        }
+    }, [currentPage, totalPages, navigate]);
 
     return (
         <div>
@@ -31,20 +66,39 @@ function TodoList({ todoList, onCompleteTodo, onUpdateTodo, isLoading }) {
                 // if no items, display instructions to add todo
                 <p>Add a todo above to get started</p>
             ) : (
-                // if items, display items
-                <ul className={classes.itemList}>
-                    {filteredTodoList.map((todo) => (
-                        <TodoListItem
-                            key={todo.id}
-                            todo={todo}
-                            onCompleteTodo={onCompleteTodo}
-                            onUpdateTodo={onUpdateTodo}
-                        />
-                    ))}
-                </ul>
+                <div>
+                    {/* // if items, display items */}
+                    <ul className={classes.itemList}>
+                        {filteredTodoList.map((todo) => (
+                            <TodoListItem
+                                key={todo.id}
+                                todo={todo}
+                                onCompleteTodo={onCompleteTodo}
+                                onUpdateTodo={onUpdateTodo}
+                            />
+                        ))}
+                    </ul>
+                    <div className={classes.paginationControls}>
+                        <button
+                            className={classes.paginationBtn}
+                            onClick={() => handlePreviousPage}
+                            disabled={currentPage === 1}
+                        >
+                            Previous
+                        </button>
+                        <span>
+                            Page {currentPage} of {totalPages}
+                        </span>
+                        <button
+                            className={classes.paginationBtn}
+                            onClick={() => handleNextPage}
+                            disabled={currentPage === totalPages}
+                        >
+                            Next
+                        </button>
+                    </div>
+                </div>
             )}
         </div>
     );
 }
-
-export default TodoList;
